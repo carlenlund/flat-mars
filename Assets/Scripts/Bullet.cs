4544ff80ecadbbe8f67652e47088276615f2cf36
Carl Enlund <carl.enlund@gmail.com>
2021-03-23 08:11:56 +0100

    Fix script locations

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+  public Vector3 Direction = new Vector3(0.0f, 0.0f, 1.0f);
+  public float Speed = 50.0f;
+
+  private float _spawnTime;
+
+  void Start() {
+    _spawnTime = Time.time;
+  }
+
+  void Update() {
+    Rigidbody rigidbody = GetComponent<Rigidbody>();
+    rigidbody.velocity = Direction * Speed;
+
+    if (Time.time - _spawnTime > 1.0f) {
+      gameObject.SetActive(false);
+    }
+  }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+  public GameObject Target;
+  public float Speed = 1.0f;
+
+  void Start() {
+  }
+
+  void Update() {
+    transform.LookAt(Target.transform);
+
+    transform.position += transform.forward * Speed * Time.deltaTime;
+  }
+}
